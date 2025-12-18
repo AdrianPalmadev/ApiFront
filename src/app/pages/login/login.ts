76612f244f5fa5624c05fafd37320cef954d5a29
itsfirestorm <itsfirestorm244@gmail.com>
2025-12-18 16:48:46 +0100

    Finished login page and added nurse model and nursedata service

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,14 +1,66 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NurseData } from '../../nursedata';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
+  selector: 'app-nurse-login',
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  form = false;
-  submit_form() {
-    this.form = true;
+  email = ''
+  password = ''
+
+  login_message: string[] = [];
+  message_type = 'error'
+  submit = false;
+
+  constructor (
+    private nurseData: NurseData,
+    private router: Router
+  ) {}
+  
+  handleFormSubmit() {
+    this.submit = true;
+    this.login_message = [];
+
+    if (this.email.length <= 0) {
+      this.login_message.push('Email cannot be empty.')
+    }
+
+    if (!this.validateEmail(this.email)) {
+      this.login_message.push('Email formatted incorrectly.')
+    }
+
+    if (this.password.length < 8) {
+      this.login_message.push('Password must be at least 8 characters long.')
+    }
+
+    if (this.login_message.length > 0) {
+      return;
+    }
+
+    // Login method returns a boolean, this is used here to verify if the login went smoothly.
+    // The user is automatically logged in, as the method already does the process before returning true or false.
+    const success = this.nurseData.login(this.email, this.password);
+
+    if (!success) {
+      this.login_message = ["Invalid email or password."]
+      return;
+    }
+
+    // After everything, it returns home.
+    this.router.navigate(['']);
   }
+
+  validateEmail(email: string): boolean {
+    return (
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.toLowerCase())
+    );
+  }
+
 }
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [Header],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
