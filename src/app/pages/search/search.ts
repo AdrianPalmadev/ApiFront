3334d867eaddbdd7044e649a86856ba6a4b4b0eb
Fire <38797905+itsfirestorm@users.noreply.github.com>
2025-12-21 22:56:30 +0100

    Nurse search

--- src/app/pages/search/search.ts
+++ src/app/pages/search/search.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Header } from '../../components/header/header';
+import { NurseData } from '../../nursedata';
+import { Nurse } from '../../nurse';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [Header],
+  imports: [Header, CommonModule, FormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
 export class Search {
+  term: string = '';
 
+  constructor(
+    public nurseService: NurseData
+  ) {}
+
+  get filteredNurses(): Nurse[]
+  {
+    const term = this.term.toLowerCase();
+
+    return this.nurseService.getNurses().filter(n => n.name.toLowerCase().includes(term));
+  }
 }
