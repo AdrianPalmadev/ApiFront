d03c66804c458adf415431a8d62694484b197b26
AdrianPalmadev <adrianmilwid@gmail.com>
2026-01-20 15:39:06 +0100

    Added loading and errors on the index

--- src/app/pages/index/index.ts
+++ src/app/pages/index/index.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Header } from "../../components/header/header";
+import { Header } from '../../components/header/header';
 import { Nurse } from '../../nurse';
 import { NurseData } from '../../nursedata';
 
@@ -10,16 +10,24 @@ import { NurseData } from '../../nursedata';
   styleUrl: './index.css',
 })
 export class Index implements OnInit {
-
   nurses: Nurse[] = [];
+
+  errorMessage: string | null = null;
+  loading = true;
 
   constructor(private nurseservice: NurseData) {}
 
   ngOnInit(): void {
-    this.nurseservice.getAllNurses()
-      .subscribe(result => {
+    this.nurseservice.getAllNurses().subscribe({
+      next: (result) => {
         this.nurses = result;
-        console.log(result);
-      });
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error(error);
+        this.errorMessage = 'Error al cargar las nurses. Inténtalo más tarde.';
+        this.loading = false;
+      },
+    });
   }
 }
